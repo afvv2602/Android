74937440220bb2af778f64f9487b8c0db15452a1
afvv2602 <afvv26002@gmail.com>
2020-11-12 15:13:28 +0100

    subida proyectos

---
+++ Layouts/app/src/main/java/mi/app/layouts/MainActivity.java
@@ -0,0 +1,88 @@
+package mi.app.layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button contraint = (Button) findViewById(R.id.constraint);
+        final Button frame = (Button) findViewById(R.id.frame);
+        final Button linear = (Button) findViewById(R.id.linear);
+        final Button relative = (Button) findViewById(R.id.relative);
+        final Button table = (Button) findViewById(R.id.table);
+
+        contraint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent comunicacion = new Intent(v.getContext(), constraint.class);
+                startActivity(comunicacion);
+            }
+        });
+        frame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent comunicacion = new Intent(v.getContext(), frame.class);
+                startActivity(comunicacion);
+            }
+        });
+        relative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent comunicacion = new Intent(v.getContext(), relative.class);
+                startActivity(comunicacion);
+            }
+        });
+        table.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent comunicacion = new Intent(v.getContext(), table.class);
+                startActivity(comunicacion);
+            }
+        });
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent comunicacion = new Intent(v.getContext(), linear.class);
+                startActivity(comunicacion);
+            }
+        });
+    }
+}
+class constraint extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constrainlayout);
+    }
+}
+class frame extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.framelayout);
+    }
+}
+class table extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelayout);
+    }
+}
+class linear extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout);
+    }
+}
+class relative extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relativelayout);
+    }
+}
